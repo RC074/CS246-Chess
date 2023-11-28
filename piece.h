c0b8859252f485a615fa980b01224a4540a46e11
Richard <clouds.overflow@gmail.com>
2023-11-28 15:05:47 -0500

    .

--- piece.h
+++ piece.h
@@ -1,8 +1,42 @@
-#ifndef __PIECE_H__
-#define __PIECE_H__
+// Piece.h
+#ifndef PIECE_H
+#define PIECE_H
 
-class Piece {
+#include <vector>
 
+enum class PieceType { KING, QUEEN, BISHOP, KNIGHT, ROOK, PAWN, NONE };
+enum class Color { WHITE, BLACK, NO_COLOR };
+
+struct Move {
+    int row, col;
+    Piece *p;
 };
 
-#endif
+class Piece {
+  int row;
+  int col;
+public:
+    Piece(int row, int c);
+    virtual ~Piece() = default;
+    virtual void notify(Piece &p) = 0;
+    virtual PieceType pieceType() const = 0;
+    virtual Color getColor() const = 0;
+    virtual std::vector<Move> getPossibleMoves() const = 0;
+    // Other methods can be declared here
+};
+
+class King : public Piece {
+public:
+    std::vector<Move> getPossibleMoves() const override;
+    // Other King-specific methods and members
+};
+
+class Queen : public Piece {
+public:
+    std::vector<Move> getPossibleMoves() const override;
+    // Other Queen-specific methods and members
+};
+
+// ... Other pieces follow a similar pattern ...
+
+#endif // PIECE_H
