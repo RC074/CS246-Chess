d7a5ff691f3647097de8a05afb9e202e947ff59c
zxTomw <tom_zhixing@outlook.com>
2023-11-28 16:41:51 -0500

    yay

--- piece.h
+++ piece.h
@@ -16,22 +16,12 @@ struct Move {
 class Piece {
   int row;
   int col;
-<<<<<<< HEAD
-  PieceType pt;
-=======
   Color color;
   std::vector<Observer> observers;
->>>>>>> e6d26f07425d90094ec30c683540acec27f8cd8e
 public:
     Piece(int row, int col, Color color);
     virtual ~Piece() = default;
     virtual void notify(Piece &p) = 0;
-<<<<<<< HEAD
-    PieceType pieceType() const;
-    Color getColor() const;
-    virtual std::vector<Move> getPossibleMoves() const = 0;
-    // Other methods can be declared here
-=======
     virtual PieceType pieceType() const = 0;
     Color getColor() const;
     int getRow() const;
@@ -39,7 +29,6 @@ public:
     void setPosition(int r, int c);
     void notifyAllObservers();
     virtual std::vector<Move> getPossibleMoves(std::vector<std::vector<Piece> > board) const = 0;
->>>>>>> e6d26f07425d90094ec30c683540acec27f8cd8e
 };
 
 #endif
