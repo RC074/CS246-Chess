25334581c79ea814fdd81697c3464ffdd334cd76
zxTomw <tom_zhixing@outlook.com>
2023-11-30 15:10:36 -0500

    test

--- test.cc
+++ test.cc
@@ -1,10 +1,20 @@
 #include <iostream>
 #include "piece.h"
 #include "king.h"
+#include "board.h"
+#include "computer.h"
 
 using namespace std;
 
+Computer p1 {};
+Computer p2 {};
+
+Board board {&p1, &p2};
+
 int main(int argc, char const *argv[]) {
-  King k = King{1, 2, Color::WHITE};
-  cout << k.getCol();
+  board.getPieceAt({0,3})->setPosition(3, 3);
+  board.move(board.getPieceAt({3,3}), 5, 5);
+  if (board.getPieceAt({5, 5})->pieceType() == PieceType::QUEEN) {
+    cout << 22222 << endl;
+  }
 }
