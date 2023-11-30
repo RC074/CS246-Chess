3056aa42dbe27d3573c5878eaeb11891dcb1d8cb
zxTomw <tom_zhixing@outlook.com>
2023-11-30 15:10:51 -0500

    test

--- test.cc
+++ test.cc
@@ -12,9 +12,9 @@ Computer p2 {};
 Board board {&p1, &p2};
 
 int main(int argc, char const *argv[]) {
-  board.getPieceAt({0,3})->setPosition(3, 3);
-  board.move(board.getPieceAt({3,3}), 5, 5);
-  if (board.getPieceAt({5, 5})->pieceType() == PieceType::QUEEN) {
+  board.getPieceAt(0,3)->setPosition(3, 3);
+  board.move(board.getPieceAt(3,3), 5, 5);
+  if (board.getPieceAt(5,5)->pieceType() == PieceType::QUEEN) {
     cout << 22222 << endl;
   }
 }
