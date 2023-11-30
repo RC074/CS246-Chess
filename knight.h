9934306a7c726da5eabfaa381fd2fe955f681db4
Richard <clouds.overflow@gmail.com>
2023-11-30 13:41:39 -0500

    .

---
+++ knight.h
@@ -0,0 +1,16 @@
+// Knight.h
+#ifndef KNIGHT_H
+#define KNIGHT_H
+
+#include "piece.h"
+
+class Knight : public Piece {
+public:
+    Knight(int row, int col, Color color);
+    ~Knight() override;
+    std::vector<Move> getPossibleMoves(std::vector<std::vector<Piece*> > board) const override;
+    PieceType pieceType() const override;
+    void notify(Piece &p) override;
+};
+
+#endif // KNIGHT_H
