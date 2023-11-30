a465a039675b020267efd36344d038ae2c13a402
Richard <clouds.overflow@gmail.com>
2023-11-30 16:14:22 -0500

    .

--- test.cc
+++ test.cc
@@ -1,19 +1,34 @@
 #include <iostream>
-#include "piece.h"
-#include "king.h"
 #include "board.h"
+#include "player.h"
 #include "computer.h"
 
 using namespace std;
 
-Computer p1 {};
-Computer p2 {};
+Computer p1;
+Computer p2;
 
 Board board {&p1, &p2};
 
 int main(int argc, char const *argv[]) {
-  board.getPieceAt(0,3)->setPosition(3, 3);
-  board.move(board.getPieceAt(3,3), 5, 5);
+  cout << "HIHHIHI" << endl;
+  board.init(true);
+  Piece *p = board.getPieceAt(0,3);
+  if (p) {
+    cout << 3333 << endl;
+  }
+  cout << "HIHHIHI" << endl;
+  if (p->pieceType() == PieceType::QUEEN) {
+    cout << 22222 << endl;
+  }
+  cout << "HIHHIHI" << endl;
+  p->setPosition(3, 3);
+  cout << "HIHHIHI" << endl;
+  board.move(p, 5, 5);
+  cout << "HIHHIHI" << endl;
+  if (board.getPieceAt(5,5)) {
+    cout << 666 << endl;
+  }
   if (board.getPieceAt(5,5)->pieceType() == PieceType::QUEEN) {
     cout << 22222 << endl;
   }
