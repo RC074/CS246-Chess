f5c028da70619c203ebf1a908b91ab1da4917702
Tom Wang <z75wang@uwaterloo.ca>
2023-12-04 14:56:36 -0500

    update computer

--- computer.cc
+++ computer.cc
@@ -5,8 +5,7 @@ PlayerType Computer::playerType() const {
 }
 
 Move Computer::getNextMove(istream &in) const {
-    auto opponentColor = (getColor() == Color::BLACK) ? Color::WHITE : Color::BLACK;
-    auto threat = getBoard()->getDangerZone(opponentColor);
+    vector<vector<bool>> threat = getThreatBoard();
     Move bestMove = {0, 0, 0, 0};
     int bestScore = -1;
     auto board = getBoard()->getBoard();
@@ -14,8 +13,6 @@ Move Computer::getNextMove(istream &in) const {
         for (auto piece:row) {
             if (!piece || piece->getColor() != getColor()) continue;
             auto moves = piece->getPossibleMoves(board);
-            int r = piece->getRow();
-            int c = piece->getCol();
             for (auto move:moves) {
                 if (move.r1 < 0 || move.r1 > 8 || move.c1 < 0 || move.c1 > 8) {
                     continue;
@@ -28,8 +25,22 @@ Move Computer::getNextMove(istream &in) const {
             }
         }
     }
-    std::cout << bestMove.c0 << bestMove.r0 << std::endl;
     return bestMove;
+}
+
+vector<vector<bool>> Computer::getThreatBoard() const {
+    auto theBoard = getBoard()->getBoard();
+    vector<vector<bool>> threat (8, vector<bool> (8, false));
+    for (int row = 0; row < BOARD_SIZE; ++row) {
+        for (int col = 0; col < BOARD_SIZE; ++col) {
+            auto piece = theBoard[row][col];
+            if (!piece || piece->getColor() == getColor()) continue;
+            for (auto move:theBoard[row][col]->getPossibleMoves(theBoard)) {
+                threat[move.r1][move.c1] = true;
+            }
+        }
+    }
+    return threat;
 }
 
 int Level1::rankMove(const Move &move, BoolBoard &threat) const {
@@ -46,15 +57,21 @@ int Level2::rankMove(const Move &move, BoolBoard &threat) const {
 }
 
 int Level3::rankMove(const Move &move, BoolBoard &threat) const {
-    if (!threat[move.r1][move.c1]) return 0;
-    if (!threat[move.r0][move.c0]) return 11;
+    if (threat[move.r1][move.c1]) return 0;
+    if (threat[move.r0][move.c0]) return 11;
     if (move.captures) {
-        if (move.captures->pieceType() == PieceType::KING) return 10;
+        if (move.captures->pieceType() == PieceType::KING) return 12;
         return 9;
     }
     return getColor() == Color::BLACK? move.r1 + 1 : 8-move.r1;
 }
 
 int Level4::rankMove(const Move &move, BoolBoard &threat) const {
-
+    if (threat[move.r1][move.c1]) return 0;
+    if (threat[move.r0][move.c0]) return 11;
+    if (move.captures) {
+        if (move.captures->pieceType() == PieceType::KING) return 12;
+        return 9;
+    }
+    return getColor() == Color::BLACK? move.r1 + 1 : 8-move.r1;
 }
--- pawn.h
+++ pawn.h
@@ -8,7 +8,8 @@ class Pawn : public Piece {
 public:
     Pawn(int row, int col, Color color);
     ~Pawn() override;
-    std::vector<Move> getPossibleMoves(std::vector<std::vector<Piece*> > board) const override;
+    std::vector<Move> getPossibleMoves(std::vector<std::vector<Piece*>> board) const override;
+    std::vector<Move> getThreat(std::vector<std::vector<Piece*>> board);
     PieceType pieceType() const override;
     void notify(Piece &p) override;
 };
